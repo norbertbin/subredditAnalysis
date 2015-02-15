a0ec9016fccd04f60e698c9c00a9165bc3325124
Norbert Binkiewicz <norbert@stat.wisc.edu>
2015-02-14 21:11:17 -0600

    added a class for comments

---
+++ comment.py
@@ -0,0 +1,77 @@
+import sqlite3 as lite
+
+class Comment:
+    """A class for scrapped reddit comments"""
+    def __init__(self, comment_id, author, created_utc, score, num_replies, text):
+        self.comment_id = comment_id
+        self.author = author
+        self.created_utc = created_utc
+        self.score = score
+        self.num_replies = num_replies
+        self.text = text
+        
+    @classmethod
+    def read_raw_com(cls, raw_comment):
+        """Creates a Comment from a praw.objects.Comment"""
+        return cls(raw_comment.id.encode('ascii', 'replace'),
+                   raw_comment.author.name.encode('ascii', 'replace'),
+                   raw_comment.created_utc,
+                   raw_comment.score,
+                   len(raw_comment.replies),
+                   raw_comment.body.encode('ascii', 'replace'))
+
+    @classmethod
+    def read_db(cls, db_name, row):
+        """Creates a Comment by reading from a database"""
+        con = lite.connection(db_name)
+        
+        with con:
+            
+            cur = con.cursor()
+            cur.execute("SELECT * FROM Comments")
+            com_tuple = cur.fetchone()
+
+        return cls(*com_tuple)
+
+    @classmethod
+    def create_table(self, db_name):
+        """Creates a Comments table in a database if it does not exist"""
+        con = lite.connect(db_name)
+
+        with con:
+
+            cur = con.cursor()
+            cur.execute("CREATE TABLE IF NOT EXISTS Comments(comment_id TEXT, "
+                        "author TEXT, created_utc INTEGER, score INTEGER, "
+                        "num_comments INTEGER, text TEXT)")
+
+    @classmethod
+    def write_db_list(comment_list, db_name):
+        """Writes a Comment list to a database efficiently"""
+        tuple_list = []
+        for comment in comment_list:
+            tuple_list.append(comment.get_tuple)
+        
+        con = lite.connection(db_name)
+
+        with con:
+
+            cur = con.cursor()
+            cur.execute("INSERT INTO Comments VALUES (?,?,?,?,?,?)",
+                        tuple_list)
+
+    def write_db(self, db_name):
+        """Writes the Comment to a database"""
+        con = lite.connection(db_name)
+
+        with con:
+
+            cur = con.cursor()
+            cur.execute("INSERT INTO Comments VALUES (?,?,?,?,?,?)",
+                        (self.comment_id, self.author, self.created_utc, 
+                         self.score, self.num_replies, self.text))
+
+    def get_tuple(self):
+        """Returns a tuple containing the comment data"""
+        return((self.comment_id, self.author, self.created_utc, self.score,
+                         self.num_replies, self.text))
